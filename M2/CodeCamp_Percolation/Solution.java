2be9334b39e4f0fd3d722a05602ff2246ee224d8
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-09-25 20:32:57 +0530

    "check_style_score -> 24 of 24 passed. style score: 0/1 "

--- M2/CodeCamp_Percolation/Solution.java
+++ M2/CodeCamp_Percolation/Solution.java
@@ -4,55 +4,55 @@ class Solution {
 		Scanner sc = new Scanner(System.in);
 		int input = Integer.parseInt(sc.nextLine());
 		percolate obj = new percolate(input);
-		while(sc.hasNextLine()){
-			String[] inputs = sc.nextLine().split(" ");
-			obj.open(Integer.parseInt(inputs[0])-1, Integer.parseInt(inputs[1])-1);
+		while (sc.hasNextLine()) {
+			String[] input1 = sc.nextLine().split(" ");
+			obj.open(Integer.parseInt(input1[0]) - 1, Integer.parseInt(input1[1]) - 1);
 		}
 		System.out.println(obj.ispercolate());
 	}
 
 }
 
-class percolate{
+class percolate {
 	boolean[][] grid;
 	WeightedQuickUnionUF obj;
 	int size;
-	percolate(int size){
+	percolate(int size) {
 		this.size = size;
 		grid = new boolean[size][size];
 		obj = new WeightedQuickUnionUF(size * size + 2);
 	}
-	public void open(int row, int column){
-		if(grid[row][column]){
+	public void open(int row, int column) {
+		if (grid[row][column]) {
 			return;
 		}
 		grid[row][column] = true;
-		if(row == 0){
-			obj.union(convert(row,column), size*size);
+		if (row == 0) {
+			obj.union(convert(row, column), size * size);
 		}
-		if(row == size-1){
-			obj.union(convert(row, column), size*size+1);
+		if (row == size - 1) {
+			obj.union(convert(row, column), size * size + 1);
 		}
-		if(row < size - 1 && grid[row+1][column]){ //bottom element
-			obj.union(convert(row,column), convert(row+1, column));
+		if (row < size - 1 && grid[row + 1][column]) { //bottom element
+			obj.union(convert(row, column), convert(row + 1, column));
 		}
-		if(row > 0 && grid[row-1][column]){ //top element
-			obj.union(convert(row,column), convert(row-1, column));
+		if (row > 0 && grid[row - 1][column]) { //top element
+			obj.union(convert(row, column), convert(row - 1, column));
 		}
-		if(column > 0 && grid[row][column-1]){ //left element
-			obj.union(convert(row,column), convert(row, column-1));
+		if (column > 0 && grid[row][column - 1]) { //left element
+			obj.union(convert(row, column), convert(row, column - 1));
 		}
-		if(column < size - 1 && grid[row][column+1]){ //right element
-			obj.union(convert(row,column), convert(row, column+1));
+		if (column < size - 1 && grid[row][column + 1]) { //right element
+			obj.union(convert(row, column), convert(row, column + 1));
 		}
 
 
 	}
-	public boolean ispercolate(){
-		return obj.connected(size * size, size*size+1);
+	public boolean ispercolate() {
+		return obj.connected(size * size, size * size + 1);
 	}
 
-	public int convert(int i, int j){
+	public int convert(int i, int j) {
 		return i * size + j;
 	}
 }
