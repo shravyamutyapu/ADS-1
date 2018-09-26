917830eaad4060cecbb408b221c7ea4a09cd6d50
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-09-26 16:40:24 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- M3/3-sum/Solution.java
+++ M3/3-sum/Solution.java
@@ -18,7 +18,7 @@ class Solution {
 		int count = 0;
 		for(int i = 0;i<size;i++){
 			for(int j =i;j<size;j++){
-				for(int k = j;k<size;k++ ){
+				for(int k = 1;k<size;k++ ){
 				if(arr[i]+arr[j]+arr[k] == 0) {
 					count++;
 				}
