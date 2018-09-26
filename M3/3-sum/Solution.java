93f6c40848c0b02d731db2c0c8793f1455aa52f9
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-09-26 23:15:06 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- M3/3-sum/Solution.java
+++ M3/3-sum/Solution.java
@@ -1,55 +1,57 @@
 import java.util.Scanner;
 import java.util.Arrays;
-class Solution {
-	// int size;
-	// int[] arr;
-	// // Solution(int[] arr,int size1){
-	// 	arr = new int[size];
-	// 	size = 0;
-	// }
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		Integer size = Integer.parseInt(sc.nextLine());
-		long[] arr = new long[size];
-		for (int i = 0; i < size; i++) {
-			arr[i] = Long.parseLong(sc.nextLine());
 
-		}
-		long count = 0;
-		for (int i = 0; i < size; i++) {
-			for (int j = i + 1; j < size; j++) {
-				for (int k = j + 1; k < size; k++ ) {
-					if (arr[i] + arr[j] + arr[k] == 0) {
-						count++;
-					}
+final class Solution {
 
-				}
-			}
-			//System.out.println(Arrays.toString(arr));
-		}
-		System.out.println(count);
-	}
-	// public String toString() {
-//    // Replace the code below
-//    if (size == 0) {
-//      return "[]";
-//    }
-//    String str = "[";
-//    int i = 0;
-//    for (i = 0; i < size - 1; i++) {
-//      str = str + list[i] + ",";
-//    }
-//    str = str + list[i] + "]";
-//    return str;
-
-	// }
+private Solution() {
 }
-//    String toString(){
-// 	int[] arr = "[";
-// 	for(int i=0;i<size;i++){
-// 		arr[i] = i +" ";
 
-// 	}
-// 	arr[size-1] = "]";
-// }
-// }
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Sum sum = new Sum();
+        int size = Integer.parseInt(sc.nextLine());
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = Integer.parseInt(sc.nextLine());
+        }
+        sum.countMethod(arr);
+    }
+}
+
+class Sum {
+
+    public void countMethod(final int[] arr1) {
+        int count = 0;
+        Arrays.sort(arr1);
+        int size = arr1.length;
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = i + 1; j < size; j++) {
+                int temp = -(arr1[i] + arr1[j]);
+                    if (binarySearch(arr1, temp, j + 1) != -1) {
+                        count++;
+                    }
+            }
+        }
+        System.out.println(count);
+    }
+
+public static int binarySearch(final int[] a, final int key, final int low1) {
+        int hi = a.length - 1;
+        int lo = low1;
+         while (lo <= hi) {
+         int mid = lo + (hi - lo) / 2;
+         if (key < a[mid]) {
+            hi = mid - 1;
+         } else if (key > a[mid]) {
+            lo = mid + 1;
+        } else {
+        return mid;
+        }
+    }
+    return -1;
+    }
+}
+
+
+
+
