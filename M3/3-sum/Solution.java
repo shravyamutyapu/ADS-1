58732b9a6600082c26f899615cf544f95cde2b91
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-09-26 17:48:42 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- M3/3-sum/Solution.java
+++ M3/3-sum/Solution.java
@@ -9,47 +9,47 @@ class Solution {
 	// }
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int size = Integer.parseInt(sc.nextLine());
+		Integer size = Integer.parseInt(sc.nextLine());
 		long[] arr = new long[size];
 		for (int i = 0; i < size; i++) {
 			arr[i] = Long.parseLong(sc.nextLine());
 
 		}
 		long count = 0;
-		for(int i = 0;i<size;i++){
-			for(int j =i+1;j<size;j++){
-				for(int k = j+1;k<size;k++ ){
-				if(arr[i]+arr[j]+arr[k] == 0) {
-					count++;
+		for (int i = 0; i < size; i++) {
+			for (int j = i + 1; j < size; j++) {
+				for (int k = j + 1; k < size; k++ ) {
+					if (arr[i] + arr[j] + arr[k] == 0) {
+						count++;
+					}
+
 				}
-
 			}
+			//System.out.println(Arrays.toString(arr));
 		}
-		//System.out.println(Arrays.toString(arr));
+		System.out.println(count);
 	}
-	System.out.println(count);
-}
 	// public String toString() {
- //    // Replace the code below
- //    if (size == 0) {
- //      return "[]";
- //    }
- //    String str = "[";
- //    int i = 0;
- //    for (i = 0; i < size - 1; i++) {
- //      str = str + list[i] + ",";
- //    }
- //    str = str + list[i] + "]";
- //    return str;
+//    // Replace the code below
+//    if (size == 0) {
+//      return "[]";
+//    }
+//    String str = "[";
+//    int i = 0;
+//    for (i = 0; i < size - 1; i++) {
+//      str = str + list[i] + ",";
+//    }
+//    str = str + list[i] + "]";
+//    return str;
 
 	// }
 }
- //    String toString(){
-	// 	int[] arr = "[";
-	// 	for(int i=0;i<size;i++){
-	// 		arr[i] = i +" ";
+//    String toString(){
+// 	int[] arr = "[";
+// 	for(int i=0;i<size;i++){
+// 		arr[i] = i +" ";
 
-	// 	}
-	// 	arr[size-1] = "]";
-	// }
-	// }
+// 	}
+// 	arr[size-1] = "]";
+// }
+// }
