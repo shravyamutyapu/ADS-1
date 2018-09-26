94cc89fb355b4cbeb043e3b46c9aa5d28b4fdb85
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-09-26 23:19:21 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- M3/3-sum/Solution.java
+++ M3/3-sum/Solution.java
@@ -4,12 +4,14 @@ import java.util.Arrays;
  * Solution class.
  */
 final class Solution {
-	/**
-	 * .
-	 */
-private Solution() {
-}
-
+    /**
+     * .
+     */
+    private Solution() {
+    }
+    /**.
+     * @param args [description]
+     */
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         Sum sum = new Sum();
@@ -25,6 +27,10 @@ private Solution() {
  * class Sum.
  */
 class Sum {
+    /**
+     * countmethod.
+     * @param arr1 [description]
+     */
 
     public void countMethod(final int[] arr1) {
         int count = 0;
@@ -33,28 +39,34 @@ class Sum {
         for (int i = 0; i < size - 1; i++) {
             for (int j = i + 1; j < size; j++) {
                 int temp = -(arr1[i] + arr1[j]);
-                    if (binarySearch(arr1, temp, j + 1) != -1) {
-                        count++;
-                    }
+                if (binarySearch(arr1, temp, j + 1) != -1) {
+                    count++;
+                }
             }
         }
         System.out.println(count);
     }
-
-public static int binarySearch(final int[] a, final int key, final int low1) {
+    /**
+     * binarySearch.
+     * @param a [description]
+     * @param key [description]
+     * @param low1 [description]
+     * @return [description]
+     */
+    public static int binarySearch(final int[] a, final int key, final int low1) {
         int hi = a.length - 1;
         int lo = low1;
-         while (lo <= hi) {
-         int mid = lo + (hi - lo) / 2;
-         if (key < a[mid]) {
-            hi = mid - 1;
-         } else if (key > a[mid]) {
-            lo = mid + 1;
-        } else {
-        return mid;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (key < a[mid]) {
+                hi = mid - 1;
+            } else if (key > a[mid]) {
+                lo = mid + 1;
+            } else {
+                return mid;
+            }
         }
-    }
-    return -1;
+        return -1;
     }
 }
 
