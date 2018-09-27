2a3a33aa02411d35a700081a1acd8406544d6b88
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-09-27 17:55:50 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- M4/4.1 Balanced Paranthesis/Solution.java
+++ M4/4.1 Balanced Paranthesis/Solution.java
@@ -1,31 +1,60 @@
 import java.util.Scanner;
+/**
+ * linkedlist class.
+ */
 class LinkedStackOfStrings {
+	/**
+	 * node.
+	 */
 	private Node first = null;
+	/**
+	 * inner class.
+	 */
 	private class Node {
 		char item;
 		Node nextAddress;
 	}
+	/**
+	 * returns top of the stack.
+	 */
 	public char top() {
 		return first.item;
 	}
-
-
+	/**
+	 * checks if it is empty.
+	 * @return boolean value.[description]
+	 */
 	public boolean isEmpty() {
 		return first == null;
 	}
+	/**
+	 * push method.
+	 * @param item [description]
+	 */
 	public void push(char item) {
+
 		Node oldfirst = first;
 		first = new Node();
 		first.item = item;
 		first.nextAddress = oldfirst;
 	}
+	/**
+	 * pop method that returns char.
+	 */
 	public char pop() {
 		char item = first.item;
 		first = first.nextAddress;
 		return item;
 	}
 }
+/**
+ * Solution class.
+ */
 public class Solution {
+	/**
+	 * main method.
+	 * @param args [description]
+	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int size = Integer.parseInt(sc.nextLine());
