bfeae8a5396f9b0b9a50dd378179ac5bbdeabd54
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-09-27 16:43:01 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ M4/4.1 Balanced Paranthesis/Solution.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+class LinkedStackOfStrings {
+	private Node first = null;
+	private class Node {
+		char item;
+		Node nextAddress;
+	}
+	public char top() {
+		return first.item;
+	}
+
+
+	public boolean isEmpty() {
+		return first == null;
+	}
+	public void push(char item) {
+		Node oldfirst = first;
+		first = new Node();
+		first.item = item;
+		first.nextAddress = oldfirst;
+	}
+	public char pop() {
+		char item = first.item;
+		first = first.nextAddress;
+		return item;
+	}
+}
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int size = Integer.parseInt(sc.nextLine());
+		for (int i = 0; i < size; i++) {
+			String line = sc.nextLine();
+		}
+	}
+	public static boolean checkParanthesis(String s) {
+		LinkedStackOfStrings obj = new LinkedStackOfStrings();
+		int sLength = s.length();
+		for (int i = 0; i < sLength; i++) {
+			char ch = s.charAt(i);
+			if (ch == '(' || ch == '{' || ch == '[') {
+				obj.push(ch);
+			}
+			if (ch == ')' && obj.top() == '(') {
+				obj.pop();
+			} else if (ch == '}' && obj.top() == '{') {
+				obj.pop();
+			} else if (ch == ']' && obj.top() == '[') {
+				obj.pop();
+			} else return false;
+		}
+		return obj.isEmpty();
+	}
+}
