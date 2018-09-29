6d8ac851ab9b32907cffffe396e50d0f12182dd2
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-09-29 12:59:09 +0530

    "check_style_score -> 6 of 12 passed. style score: 0/1 "

--- M6/Assignment-1/Solution.java
+++ M6/Assignment-1/Solution.java
@@ -3,36 +3,32 @@ import java.util.Scanner;
  * classAddLargenumbers.
  */
 final class AddLargeNumbers {
-	/**
-	 * default private constructor.
-	 */
+    /**
+     * default private constructor.
+     */
 
-	private AddLargeNumbers() {
-	}
-	/**
-	 * @brief [brief description]
-	 * @details [long description]
-	 *
-	 * @param args [description]
-	 * @return linkedlist [description]
-	 */
+    private AddLargeNumbers() {
+    }
+    /**
+     * @return linkedlist [description]
+     */
 
-	public static LinkedList numberToDigits(final String number) {
-		LinkedList link = new LinkedList();
-		for (int i = number.length() - 1; i >= 0; i--) {
-			String str = "" + number.charAt(i);
-			link.push(Integer.parseInt(str));
-		}
-		return link;
-	}
-	/**
-	 * @param list [description]
-	 * @return String [description]
-	 */
+    public static LinkedList numberToDigits(final String number) {
+        LinkedList link = new LinkedList();
+        for (int i = number.length() - 1; i >= 0; i--) {
+            String str = "" + number.charAt(i);
+            link.push(Integer.parseInt(str));
+        }
+        return link;
+    }
+    /**
+     * @param list [description]
+     * @return String [description]
+     */
 
-	public static String digitsToNumber(final LinkedList list) {
-		return list.toString();
-	}
+    public static String digitsToNumber(final LinkedList list) {
+        return list.toString();
+    }
 
 }
 /**
@@ -43,34 +39,34 @@ public final class Solution {
     /**
      * default private constructor.
      */
-	private Solution() {
-	}
-	/**
-	 * main method.
-	 * @param args [description]
-	 */
+    private Solution() {
+    }
+    /**
+     * main method.
+     * @param args [description]
+     */
 
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String input = sc.nextLine();
-		String p = sc.nextLine();
-		String q = sc.nextLine();
-		switch (input) {
-		case "numberToDigits":
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        String p = sc.nextLine();
+        String q = sc.nextLine();
+        switch (input) {
+        case "numberToDigits":
 LinkedList pDigits = AddLargeNumbers.numberToDigits(p);
 LinkedList qDigits = AddLargeNumbers.numberToDigits(q);
 System.out.println(AddLargeNumbers.digitsToNumber(pDigits));
 System.out.println(AddLargeNumbers.digitsToNumber(qDigits));
-			break;
-		// case "addLargeNumbers":
-		//     pDigits = AddLargeNumbers.numberToDigits(p);
-		//     qDigits = AddLargeNumbers.numberToDigits(q);
-		// LinkedList result =
-		// AddLargeNumbers.addLargeNumbers(pDigits, qDigits);
-		//     System.out.println(AddLargeNumbers.digitsToNumber(result));
-		//     break;
-		default:
-			break;
-		}
-	}
+            break;
+        // case "addLargeNumbers":
+        //     pDigits = AddLargeNumbers.numberToDigits(p);
+        //     qDigits = AddLargeNumbers.numberToDigits(q);
+        // LinkedList result =
+// AddLargeNumbers.addLargeNumbers(pDigits, qDigits);
+//     System.out.println(AddLargeNumbers.digitsToNumber(result));
+        //     break;
+        default:
+            break;
+        }
+    }
 }
