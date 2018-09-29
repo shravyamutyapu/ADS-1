29027d15539cef4fcfe583cc479b05b45dd1e658
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-09-29 11:29:51 +0530

    "check_style_score -> 0 of 12 passed. style score: 0/1 "

--- M6/Assignment-1/Sol1.java
+++ M6/Assignment-1/Sol1.java
@@ -13,6 +13,28 @@ class linkedlistATN
             this.val = val;
         }
     }
+    public static void main(String args[])
+    {
+        linkedlistATN list = new linkedlistATN();
+        list.head1 = null;
+        list.head2 = null;
+        list.result = null;
+        list.carry = 0;
+        int arr1[] = { 9, 9, 9 };
+        int arr2[] = { 1, 8 };
+
+        // Create first list as 9->9->9
+        for (int i = arr1.length - 1; i >= 0; --i)
+            list.push(arr1[i], 1);
+
+        // Create second list as 1->8
+        for (int i = arr2.length - 1; i >= 0; --i)
+            list.push(arr2[i], 2);
+
+        list.addlists();
+
+        list.printlist(list.result);
+    }
 
     // Function to print linked list
     void printlist(node head)
@@ -167,28 +189,7 @@ class linkedlistATN
     }
 
     // Driver program to test above functions
-    public static void main(String args[])
-    {
-        linkedlistATN list = new linkedlistATN();
-        list.head1 = null;
-        list.head2 = null;
-        list.result = null;
-        list.carry = 0;
-        int arr1[] = { 9, 9, 9 };
-        int arr2[] = { 1, 8 };
 
-        // Create first list as 9->9->9
-        for (int i = arr1.length - 1; i >= 0; --i)
-            list.push(arr1[i], 1);
-
-        // Create second list as 1->8
-        for (int i = arr2.length - 1; i >= 0; --i)
-            list.push(arr2[i], 2);
-
-        list.addlists();
-
-        list.printlist(list.result);
-    }
 }
 
 
