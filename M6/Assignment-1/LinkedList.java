3a507d05978b762799e73ee6f6d4aa9cfaf6477b
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-09-29 12:48:16 +0530

    "check_style_score -> 6 of 12 passed. style score: 0/1 "

--- M6/Assignment-1/LinkedList.java
+++ M6/Assignment-1/LinkedList.java
@@ -1,50 +1,49 @@
 class LinkedList {
-	private int length = 0;
+
+
     private Node first = null;
-    private Node last = null;
-    private class Node {
-        private String item;
+
+    public class Node {
+
+        Node() {
+        }
+
+        private int item;
+
         private Node next;
+
+        Node(final int inputItem) {
+            this.item = inputItem;
+        }
     }
+
+    public int top() {
+        return first.item;
+    }
+
+    public void push(final int item) {
+        Node nextAddress = new Node(item);
+        nextAddress.next = first;
+        first = nextAddress;
+    }
+
+    public int pop() {
+        int output = first.item;
+        first = first.next;
+        return output;
+    }
+
     public boolean isEmpty() {
         return first == null;
     }
-    public void push(final String item) {
-        if (first == null) {
-            first = new Node();
-            first.item = item;
-            first.next = null;
-            last = first;
-        } else {
-            Node oldfirst = first;
-            first = new Node();
-            first.item = item;
-            first.next = oldfirst;
+
+    public String toString() {
+        Node head = first;
+        String storage = "";
+        while (head != null) {
+            storage = storage + head.item;
+            head = head.next;
         }
-        length++;
-    }
-    public void pop() {
-        if (first != null) {
-            first = first.next;
-            length--;
-        }
-    }
-    public String top() {
-        return first.item;
-    }
-    public int size() {
-        return length;
-    }
-    public String displayAll() {
-        if (length != 0) {
-            String s = "";
-            Node temp = first;
-            while (temp != null) {
-                s += temp.item;
-                temp = temp.next;
-            }
-            return s;
-        }
-        return "";
+        return storage;
     }
 }
--- M6/Assignment-1/Solution.java
+++ M6/Assignment-1/Solution.java
@@ -1,55 +1,5 @@
 import java.util.Scanner;
 
-class LinkedList {
-
-
-    private Node first = null;
-
-    public class Node {
-
-        Node() {
-        }
-
-        private int item;
-
-        private Node next;
-
-        Node(final int inputItem) {
-            this.item = inputItem;
-        }
-    }
-
-    public int top() {
-        return first.item;
-    }
-
-    public void push(final int item) {
-        Node nextAddress = new Node(item);
-        nextAddress.next = first;
-        first = nextAddress;
-    }
-
-    public int pop() {
-        int output = first.item;
-        first = first.next;
-        return output;
-    }
-
-    public boolean isEmpty() {
-        return first == null;
-    }
-
-    public String toString() {
-        Node head = first;
-        String storage = "";
-        while (head != null) {
-            storage = storage + head.item;
-            head = head.next;
-        }
-        return storage;
-    }
-}
-
 final class AddLargeNumbers {
 
     private AddLargeNumbers() {
