20645c23c338e575ca7796795b18ce93f667c8dd
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-10-02 22:38:32 +0530

    "check_style_score -> 6 of 6 passed. style score: 1/1 "

--- M8/8.1 Team Ranking With Insertion/Solution.java
+++ M8/8.1 Team Ranking With Insertion/Solution.java
@@ -3,8 +3,14 @@ import java.util.Scanner;
  * Solution class.
  */
 public final class Solution {
-    private static final int ten = 10;
-    private static final int three = 3;
+    /**
+     * magic number.
+     */
+    private static final int TEN = 10;
+     /**
+     * magic number.
+     */
+    private static final int THREE = 3;
     /**
      * default constructor.
      */
@@ -14,14 +20,14 @@ public final class Solution {
      * @param args [description]
      */
     public static void main(final String[] args) {
-        Team[] teams = new Team[ten];
+        Team[] teams = new Team[TEN];
         int size = 0;
         Scanner scan = new Scanner(System.in);
         Table t = new Table();
         while (scan.hasNext()) {
             String[] line = scan.nextLine().split(",");
             teams[size++] = new Team(line[0], Integer.parseInt(line[1]),
-                Integer.parseInt(line[2]), Integer.parseInt(line[three]));
+                Integer.parseInt(line[2]), Integer.parseInt(line[THREE]));
         }
         System.out.println(t.leaderboard(teams, size));
     }
