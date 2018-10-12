7a82afb68b7eff3490600351e0ea0b38071f944b
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-10-12 19:20:21 +0530

    "taxi_cab_numbers: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ M17/TaxiCabNumbers/Solution.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+/**
+ * Solution class.
+ */final class Solution{
+	/**
+	 * default private constructor.
+	 */
+	private Solution() { }
+	/**
+	 * main method.
+	 * @param args.
+	 */
+	public static void main(final String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String[] values = sc.nextLine().split(" ");
+	    int combinations = Integer.parseInt(values[1]);
+	    int nthNum = Integer.parseInt(values[0]);
+
+	}
+
+}
