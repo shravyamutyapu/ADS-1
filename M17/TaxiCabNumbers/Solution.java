5811a1e441a989ea3e6283b4a5dad17b1ef9652f
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-10-12 19:56:03 +0530

    "taxi_cab_numbers: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- M17/TaxiCabNumbers/Solution.java
+++ M17/TaxiCabNumbers/Solution.java
@@ -1,14 +1,23 @@
 import java.util.Scanner;
-
 /**
  * class Solution.
  */
-class Solution {
+final class Solution {
+    /**
+     * default private constructor.
+     */
+    private Solution() { }
+    /**
+     * main method.
+     * @param args [description]
+     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        int n = 1000;
+        int n1 = sc.nextInt();
+        int m = sc.nextInt();
+        final int th = 1000;
+        int n = th;
+
 
         // initialize priority queue
         MinPQ<CubeSum> pq = new MinPQ<CubeSum>();
@@ -26,10 +35,10 @@ class Solution {
             CubeSum current = pq.delMin();
             if (current.sum == previous.sum) {
                 pair++;
-                if (pair == M) {
+                if (pair == m) {
                     nth++;
                 }
-                if (N == nth) {
+                if (n1 == nth) {
                     System.out.println(current);
                     break;
                 }
@@ -38,9 +47,9 @@ class Solution {
             }
 
             previous = current;
-            if (current.j < n)
+            if (current.j < n) {
                 pq.insert(new CubeSum(current.i, current.j + 1));
+            }
         }
     }
-
 }
