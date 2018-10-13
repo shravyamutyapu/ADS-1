6b353b0f62280cab08e5e47ac7580ee300bcb04a
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-10-13 13:14:02 +0530

    "ads1_week3_exam: 4/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- M18/Week-3_Assignment-1/Solution.java
+++ M18/Week-3_Assignment-1/Solution.java
@@ -1,50 +1,86 @@
 import java.util.Scanner;
-/**
- * Solution class.
- * @param args [description]
- */
 final class Solution {
     /**
-     * private default constructor.
+     * private default consttructor..
      */
-    private Solution() { }
+    private Solution() {
+        //unused.
+    }
     /**
      * main method.
-     * @param args [description]
+     *
+     * @param      args  The arguments
      */
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int numOfStocks = Integer.parseInt(sc.nextLine());
-        for (int i = 0; i < 6; i++) {
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        BinarySearchST<String, Integer> maxST =
+        new  BinarySearchST<>();
+        BinarySearchST<String, Integer> minST =
+        new BinarySearchST<>();
+        int numOfStacks = Integer.parseInt(scan.nextLine());
+        final int six = 6;
+        final int five = 5;
+        for (int i = 0; i < six; i++) {
             int count = 0;
             MinPQ<Stock> min = new MinPQ<>();
             MaxPQ<Stock> max = new MaxPQ<>();
-            while (count < numOfStocks) {
-                String[] stockDetails = sc.nextLine().split(",");
-                Stock stocks = new Stock(stockDetails[0],
-                                         Double.parseDouble(stockDetails[1]));
-                min.insert(stocks);
-                max.insert(stocks);
+            while (count < numOfStacks) {
+                String[] stockDetails =
+                scan.nextLine().split(",");
+                Stock stockobj = new Stock(stockDetails[0],
+                    Double.parseDouble(stockDetails[1]));
+                min.insert(stockobj);
+                max.insert(stockobj);
                 count++;
             }
-            BinarySearchST<String, Double> best = new  BinarySearchST<>();
-            BinarySearchST<String, Double> worst = new BinarySearchST<>();
-            for (int j = 0; j < 5; j++) {
-                Stock maxbest = max.delMax();
-                System.out.println(maxbest);
-                best.put(maxbest.getName(), maxbest.getpercentageDiff());
+            for (int j = 0; j < five; j++) {
+                Stock best = max.delMax();
+                if (maxST.contains(best.getName())) {
+                    int value = maxST.get(
+                        best.getName());
+                    maxST.put(best.getName(),
+                        ++value);
+                } else {
+                    maxST.put(best.getName(), 1);
+                }
+                System.out.println(best);
             }
             System.out.println();
-            for (int k = 0; k < 5; k++) {
-                Stock minworst = min.delMin();
-                System.out.println(minworst);
-                worst.put(minworst.getName(), minworst.getpercentageDiff());
+            for (int k = 0; k < five; k++) {
+                Stock worst = min.delMin();
+                if (minST.contains(worst.getName())) {
+                    int valuemin = minST.get(
+                        worst.getName());
+                    minST.put(worst.getName(),
+                        ++valuemin);
+                } else {
+                    minST.put(worst.getName(), 1);
+                }
+                System.out.println(worst);
             }
             System.out.println();
-            // int queryCount = Integer.parseInt(sc.)
         }
+        int queryLength = Integer.parseInt(scan.nextLine());
+        for (int l = 0; l < queryLength; l++) {
+            String[] queryTokens = scan.nextLine().split(",");
+            if (queryTokens[0].equals("get")) {
+                if (queryTokens[1].equals("maxST")) {
+                    if (!maxST.contains(queryTokens[2])) {
+                        System.out.println("0");
+                    } else {
+                        System.out.println(
+                            maxST.get(queryTokens[2]));
+                    }
+                } else {
+                    if (!minST.contains(queryTokens[2])) {
+                        System.out.println("0");
+                    } else {
+                        System.out.println(
+                            minST.get(queryTokens[2]));
+                    }
+                }
+            }
+        }
+
     }
 }
-
-
-
