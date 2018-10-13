6f41d96b9adefdbce3b63958ba89a22ce5a20d0b
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-10-13 12:56:25 +0530

    "ads1_week3_exam: 2/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- M18/Week-3_Assignment-1/Stock.java
+++ M18/Week-3_Assignment-1/Stock.java
@@ -2,64 +2,64 @@
  * stock class.
  */
 class Stock implements Comparable<Stock> {
-	/**
-	 * Stock Name.
-	 */
-	private String stockName;
-	/**
-	 * percentage difference.
-	 */
-	private double percentageDiff;
+    /**
+     * Stock Name.
+     */
+    private String stockName;
+    /**
+     * percentage difference.
+     */
+    private double percentageDiff;
     /**
      * constructor.
      * @param name [stock name]
      * @param diff [stock diff]
      */
-	Stock(String name, double diff) {
-		this.stockName = name;
-		this.percentageDiff = diff;
-	}
-	/**
-	 * to get stock name.
-	 * @return stockName
-	 */
-	public String getName() {
-		return stockName;
-	}
-	/**
-	 * to get percentage Difference.
-	 * @return percentageDiff
-	 */
-	public double getpercentageDiff() {
-		return percentageDiff;
-	}
-	/**
-	 * toString() for stock.
-	 * @return [String]
-	 */
-	public String toString() {
-		return stockName + " " + percentageDiff;
-	}
-	/**
-	 * compareTo method(overidden).
-	 * @param that [stock Type]
-	 * @return [int value]
-	 */
-	public int compareTo(final Stock that) {
-		if (this.percentageDiff > that.getpercentageDiff()) {
-			return 1;
-		}
-		if (this.percentageDiff < that.getpercentageDiff()) {
-			return -1;
-		}
-		if (this.stockName.compareTo(that.getName()) > 0) {
-			return 1;
-		}
-		if (this.stockName.compareTo(that.getName()) < 0) {
-			return -1;
-		} else {
-			return 0;
-		}
-	}
+    Stock(final String name, final double diff) {
+        this.stockName = name;
+        this.percentageDiff = diff;
+    }
+    /**
+     * to get stock name.
+     * @return stockName
+     */
+    public String getName() {
+        return stockName;
+    }
+    /**
+     * to get percentage Difference.
+     * @return percentageDiff
+     */
+    public double getpercentageDiff() {
+        return percentageDiff;
+    }
+    /**
+     * toString() for stock.
+     * @return [String]
+     */
+    public String toString() {
+        return stockName + " " + percentageDiff;
+    }
+    /**
+     * compareTo method(overidden).
+     * @param that [stock Type]
+     * @return [int value]
+     */
+    public int compareTo(final Stock that) {
+        if (this.percentageDiff > that.getpercentageDiff()) {
+            return 1;
+        }
+        if (this.percentageDiff < that.getpercentageDiff()) {
+            return -1;
+        }
+        if (this.stockName.compareTo(that.getName()) > 0) {
+            return 1;
+        }
+        if (this.stockName.compareTo(that.getName()) < 0) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
 
 }
