ced5553bda4476aaea6b25e8a50c1293d88467f9
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-10-13 21:58:07 +0530

    "ads1_m14_a1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- M14/14.1 Dynamic Median Finding/Solution.java
+++ M14/14.1 Dynamic Median Finding/Solution.java
@@ -45,20 +45,20 @@ public final class Solution {
 
             }
 
-            if (minpq.size() == maxpq.size()) {
-                median = (minpq.min() + maxpq.max()) / 2;
-                System.out.println(median);
-            }
+            // if (minpq.size() == maxpq.size()) {
+            //     median = (minpq.min() + maxpq.max()) / 2;
+            //     System.out.println(median);
+            // }
 
             if (maxpq.size() > minpq.size()) {
                 median = maxpq.max();
                 System.out.println(median);
             }
 
-            // if (minpq.size() > maxpq.size()) {
-            //     median = minpq.min();
-            //     System.out.println(median);
-            // }
+            if (minpq.size() > maxpq.size()) {
+                median = minpq.min();
+                System.out.println(median);
+            }
 
         }
 
