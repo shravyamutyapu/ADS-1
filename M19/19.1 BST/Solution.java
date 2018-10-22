75b3ba5521277ae28833c7c09953981e99621943
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-10-22 19:36:08 +0530

    "ads1_m19_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- M19/19.1 BST/Solution.java
+++ M19/19.1 BST/Solution.java
@@ -3,53 +3,53 @@ import java.util.Scanner;
  * solution class.
  */
 public final class Solution {
-    /**.
-     * Constructs the object.
-     */
-    private Solution() {
-      //unused.
-    }
-    /**.
-     * main method.
-     * @param args  The arguments
-     */
-    // while loop iterates until it has next line i.e N times.
-    public static void main(final String[] args) {
-        Scanner sc = new Scanner(System.in);
-        BinarySearchTree bst = new BinarySearchTree();
-        while (sc.hasNext()) {
-            String lines = sc.nextLine();
-            String[] method = lines.split(",");
-            switch (method[0]) {
-                case"put":
-                    bst.put(new Book(method[1], method[2],
-                     Float.parseFloat(method[3])),
-                      Integer.parseInt(method[4]));
-                    break;
-                case"get":
-                    System.out.println(bst.get(new Book(method[1], method[2],
-                     Float.parseFloat(method[3]))));
-                    break;
-                case"max":
-                    System.out.println(bst.max());
-                    break;
-                case"min":
-                    System.out.println(bst.min());
-                    break;
-                case"floor":
-                    System.out.println(bst.floor(new Book(method[1], method[2],
-                     Float.parseFloat(method[3]))));
-                    break;
-                case"ceiling":
-                    System.out.println(bst.ceil(new Book(method[1], method[2],
-                     Float.parseFloat(method[3]))));
-                    break;
-                case"select":
-                    System.out.println(bst.select(Integer.parseInt(method[1])));
-                    break;
-                default:
-                break;
-            }
-        }
-    }
+	/**.
+	 * Constructs the object.
+	 */
+	private Solution() {
+		//unused.
+	}
+	/**.
+	 * main method.
+	 * @param args  The arguments
+	 */
+	// while loop iterates until it has next line i.e N times.
+	public static void main(final String[] args) {
+		Scanner sc = new Scanner(System.in);
+		BinarySearchTree bst = new BinarySearchTree();
+		while (sc.hasNext()) {
+			String lines = sc.nextLine();
+			String[] method = lines.split(",");
+			switch (method[0]) {
+			case"put":
+				bst.put(new Book(method[1], method[2],
+				                 Float.parseFloat(method[2 + 1])),
+				        Integer.parseInt(method[2 + 2]));
+				break;
+			case"get":
+				System.out.println(bst.get(new Book(method[1], method[2],
+				        Float.parseFloat(method[2 + 1]))));
+				break;
+			case"max":
+				System.out.println(bst.max());
+				break;
+			case"min":
+				System.out.println(bst.min());
+				break;
+			case"floor":
+				System.out.println(bst.floor(new Book(method[1], method[2],
+				                Float.parseFloat(method[2 + 1]))));
+				break;
+			case"ceiling":
+				System.out.println(bst.ceil(new Book(method[1], method[2],
+				                 Float.parseFloat(method[2 + 1]))));
+				break;
+			case"select":
+				System.out.println(bst.select(Integer.parseInt(method[1])));
+				break;
+			default:
+				break;
+			}
+		}
+	}
 }
