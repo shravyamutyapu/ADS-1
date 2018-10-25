a8a6aa861161c3e95700e91fae2e05681901efab
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-10-25 23:16:37 +0530

    "ads1_m22_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- M22/22.1 Ransom Note/SeparateChainingHashST.java
+++ M22/22.1 Ransom Note/SeparateChainingHashST.java
@@ -1,3 +1,6 @@
+/**
+ * class.
+ */
 public class SeparateChainingHashST<Key, Value> {
     /**
      * private variable.
@@ -9,14 +12,13 @@ public class SeparateChainingHashST<Key, Value> {
     private int n;
     /**
      * private value M .
-     */                               // number of key-value pairs
+     */
     private int m;
     /**
      * array.
+     * // array of linked-list symbol tables.
      */
-    private SequentialSearchST<Key, Value>[] st;  // array of linked-list symbol tables
-
-
+    private SequentialSearchST<Key, Value>[] st;
     /**
      * Initializes an empty symbol table.
      */
@@ -28,17 +30,19 @@ public class SeparateChainingHashST<Key, Value> {
      * Initializes an empty symbol table with {@code m} chains.
      * @param m the initial number of chains
      */
-    public SeparateChainingHashST(int m) {
+    public SeparateChainingHashST(final int m) {
         this.m = m;
         st = (SequentialSearchST<Key, Value>[]) new SequentialSearchST[m];
-        for (int i = 0; i < m; i++)
+        for (int i = 0; i < m; i++){
             st[i] = new SequentialSearchST<Key, Value>();
+        }
     }
 
     // resize the hash table to have the given number of chains,
     // rehashing all of the keys
     // private void resize(int chains) {
-    //     SeparateChainingHashST<Key, Value> temp = new SeparateChainingHashST<Key, Value>(chains);
+    //     SeparateChainingHashST<Key, Value>
+    //temp = new SeparateChainingHashST<Key, Value>(chains);
     //     for (int i = 0; i < m; i++) {
     //         for (Key key : st[i].keys()) {
     //             temp.put(key, st[i].get(key));
@@ -50,8 +54,17 @@ public class SeparateChainingHashST<Key, Value> {
     // }
 
     // hash value between 0 and m-1
-    private int hash(Key key) {
-        return (key.hashCode() & 0x7fffffff) % m;
+    /**
+     * hash method.
+     * @param Key [description]
+     * @return int[description]
+     */
+    private int hash(final Key key) {
+        /**
+         * magic number.
+         */
+        final int magic = 0x7fffffff;
+        return (key.hashCode() & magic) % m;
     }
 
     /**
@@ -81,8 +94,9 @@ public class SeparateChainingHashST<Key, Value> {
      *         {@code false} otherwise
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
-    public boolean contains(Key key) {
-        if (key == null) throw new IllegalArgumentException("argument to contains() is null");
+    public boolean contains(final Key key) {
+if (key == null) throw
+    new IllegalArgumentException("argument to contains() is null");
         return get(key) != null;
     }
 
@@ -94,16 +108,20 @@ public class SeparateChainingHashST<Key, Value> {
      *         {@code null} if no such value
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
-    public Value get(Key key) {
-        if (key == null) throw new IllegalArgumentException("argument to get() is null");
+    public Value get(final Key key) {
+        if (key == null) throw
+    new IllegalArgumentException("argument to get() is null");
         int i = hash(key);
         return st[i].get(key);
     }
 
     /**
-     * Inserts the specified key-value pair into the symbol table, overwriting the old
-     * value with the new value if the symbol table already contains the specified key.
-     * Deletes the specified key (and its associated value) from this symbol table
+     * Inserts the specified key-value pair
+     * into the symbol table, overwriting the old
+     * value with the new value if the symbol
+     * table already contains the specified key.
+     * Deletes the specified key (and its associated
+     * value) from this symbol table
      * if the specified value is {@code null}.
      *
      * @param  key the key
@@ -111,7 +129,8 @@ public class SeparateChainingHashST<Key, Value> {
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
     public void put(Key key, Value val) {
-        if (key == null) throw new IllegalArgumentException("first argument to put() is null");
+        if (key == null) throw
+            new IllegalArgumentException("first argument to put() is null");
         if (val == null) {
             delete(key);
             return;
@@ -121,19 +140,23 @@ public class SeparateChainingHashST<Key, Value> {
         // if (n >= 10*m) resize(2*m);
 
         int i = hash(key);
-        if (!st[i].contains(key)) n++;
+        if (!st[i].contains(key)){
+            n++;
+        }
         st[i].put(key, val);
     }
 
     /**
-     * Removes the specified key and its associated value from this symbol table
+     * Removes the specified key and.
+     * its associated value from this symbol table
      * (if the key is in this symbol table).
      *
      * @param  key the key
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
     public void delete(Key key) {
-        if (key == null) throw new IllegalArgumentException("argument to delete() is null");
+if (key == null) throw
+    new IllegalArgumentException("argument to delete() is null");
 
         int i = hash(key);
         if (st[i].contains(key)) n--;
