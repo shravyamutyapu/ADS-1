5e9b0887b435bf7667d66536afcffe2ed8d0a3fd
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-10-25 22:52:30 +0530

    "ads1_m22_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- M22/22.1 Ransom Note/Solution.java
+++ M22/22.1 Ransom Note/Solution.java
@@ -1,41 +1,37 @@
 import java.util.Scanner;
 /**
- * Class for solution.
+ * solution class.
  */
 public final class Solution {
     /**
-     * Constructs the object.
+     * private default constructor.
      */
-    private Solution() {
-        // Unused constructor.
-    }
+    private Solution() { }
     /**
-     * {Client Program}.
      *
-     * @param      args  The arguments
+     * @param  args  The arguments
      */
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
-        SeparateChainingHashST<String, Integer> chaining;
-        chaining = new SeparateChainingHashST<String, Integer>();
+        SeparateChainingHashST<String, Integer> chaining = new
+        SeparateChainingHashST<String, Integer>();
         String tokens = scan.nextLine();
-        String[] magazine = scan.nextLine().split(" ");
-        for (int i = 0; i < magazine.length; i++) {
-            if (chaining.contains(magazine[i])) {
-                chaining.put(magazine[i], chaining.get(magazine[i]) + 1);
+        String[] magazineText = scan.nextLine().split(" ");
+        for (int i = 0; i < magazineText.length; i++) {
+            if (chaining.contains(magazineText[i])) {
+                chaining.put(magazineText[i], chaining.get(magazineText[i]) + 1);
             } else {
-                chaining.put(magazine[i], 1);
+                chaining.put(magazineText[i], 1);
             }
         }
-        String[] tobeSearched = scan.nextLine().split(" ");
-        for (int i = 0; i < tobeSearched.length; i++) {
-            if (chaining.contains(tobeSearched[i])) {
-                if (chaining.get(tobeSearched[i]) == 0) {
+        String[] ransomeNote = scan.nextLine().split(" ");
+        for (int i = 0; i < ransomeNote.length; i++) {
+            if (chaining.contains(ransomeNote[i])) {
+            if (chaining.get(ransomeNote[i]) == 0) {
                     System.out.print("No");
                     return;
                 } else {
-                    chaining.put(
-                        tobeSearched[i], chaining.get(tobeSearched[i]) - 1);
+                	chaining.put(ransomeNote[i], chaining.get(ransomeNote[i]) - 1);
                 }
             } else {
                 System.out.print("No");
