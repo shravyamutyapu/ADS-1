9c910a3a7c6005a69031be6d8e4dfc18d5e5209c
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-10-27 12:00:18 +0530

    "m24_a1_1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- M24/1/Student.java
+++ M24/1/Student.java
@@ -3,38 +3,38 @@
  * contains student details.
  */
 class Student {
-	/**
-	 * name of the student.
-	 */
-	private String name;
-	// String rollNumber;
-	/**
-	 *marks obtained by student.
-	 */
-	private Double  marks;
-	/**
-	 * returns student name.
-	 * @return studentName[description]
-	 */
-	public String getName() {
-		return name;
-	}
-	/**
-	 * returns total marks.
-	 * @return students marks.
-	 */
-	public Double getMarks() {
-		return marks;
-	}
-	/**
-	 * parameterized constructor.
-	 * @param studName [description]
-	 * @param total [description]
-	 */
-	Student(String studName, Double total) {
-		this.name = studName;
-		// this.rollNumber = rollNum;
-		this.marks = total;
-	}
+    /**
+     * name of the student.
+     */
+    private String name;
+    // String rollNumber;
+    /**
+     *marks obtained by student.
+     */
+    private Double  marks;
+    /**
+     * returns student name.
+     * @return studentName[description]
+     */
+    public String getName() {
+        return name;
+    }
+    /**
+     * returns total marks.
+     * @return students marks.
+     */
+    public Double getMarks() {
+        return marks;
+    }
+    /**
+     * parameterized constructor.
+     * @param studName [description]
+     * @param total [description]
+     */
+    Student(final String studName, final Double total) {
+        this.name = studName;
+        // this.rollNumber = rollNum;
+        this.marks = total;
+    }
 
 }
