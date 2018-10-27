9c555ef03069d3d4441a8dc35b24fefe50dca721
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-10-27 13:09:40 +0530

    "m24_a2_1: 1/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- M24/2/Solution.java
+++ M24/2/Solution.java
@@ -1,6 +1,16 @@
+/**
+ * importing scanner;
+ */
 import java.util.Scanner;
+/**
+ * solution class.
+ */
 final class Solution {
-	public static void main(String[] args) {
+	/**
+	 * main method
+	 * @param args [description]
+	 */
+	public static void main(final String[] args) {
 		Scanner sc = new Scanner(System.in);
 		LinearProbingHashST<Student, String> hash =
 		    new LinearProbingHashST<Student, String>();
@@ -8,38 +18,37 @@ final class Solution {
 		while (recordCount > 0) {
 			String[] rec = sc.nextLine().split(",");
 
-			hash.put(new Student (rec[1], Double.parseDouble(rec[2])),rec[0]);
+			hash.put(new Student (rec[1], Double.parseDouble(rec[2])), rec[0]);
 			recordCount--;
 		}
-          int queries = Integer.parseInt(sc.nextLine());
-          while (queries > 0) {
-              String[] line = sc.nextLine().split(" ");
-              if (line[0].equals("BE")) {
-                  double less = Double.parseDouble(line[1]);
-                  double great = Double.parseDouble(line[2]);
-                  for (Student each : hash.keys()) {
-                      if (each.getMarks() >= less && each.getMarks() <= great) {
-                          System.out.println(each.getName());
-                      }
-                  }
-              } else if (line[0].equals("LE")) {
-                  double value = Double.parseDouble(line[1]);
-                  for (Student each : hash.keys()) {
-                      if (each.getMarks() <= value) {
-                          System.out.println(each.getName());
-                      }
-                  }
-              } else if (line[0].equals("GE")) {
-                  double value = Double.parseDouble(line[1]);
-                  for (Student each : hash.keys()) {
-                      if (each.getMarks() >= value) {
-                          System.out.println(each.getName());
-                      }
-                  }
-              }
-              queries--;
-          }
-      }
-
-
+		int queries = Integer.parseInt(sc.nextLine());
+		while (queries > 0) {
+			String[] line = sc.nextLine().split(" ");
+			//checks for the requirement.
+			if (line[0].equals("BE")) {
+				double less = Double.parseDouble(line[1]);
+				double great = Double.parseDouble(line[2]);
+				for (Student each : hash.keys()) {
+					if (each.getMarks() >= less && each.getMarks() <= great) {
+						System.out.println(each.getName());
+					}
+				}
+			} else if (line[0].equals("LE")) {
+				double value = Double.parseDouble(line[1]);
+				for (Student each : hash.keys()) {
+					if (each.getMarks() <= value) {
+						System.out.println(each.getName());
+					}
+				}
+			} else if (line[0].equals("GE")) {
+				double value = Double.parseDouble(line[1]);
+				for (Student each : hash.keys()) {
+					if (each.getMarks() >= value) {
+						System.out.println(each.getName());
+					}
+				}
+			}
+			queries--;
+		}
+	}
 }
