ef8518ea1c37960f661f5722b9843f896b0cbc41
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-10-27 11:58:33 +0530

    "m24_a1_1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- M24/1/Student.java
+++ M24/1/Student.java
@@ -1,14 +1,37 @@
-class Student{
+/**
+ * student class that
+ * contains student details.
+ */
+class Student {
+	/**
+	 * name of the student.
+	 */
 	private String name;
 	// String rollNumber;
+	/**
+	 *marks obtained by student.
+	 */
 	private Double  marks;
-	public String getName(){
+	/**
+	 * returns student name.
+	 * @return studentName[description]
+	 */
+	public String getName() {
 		return name;
 	}
-	public Double getMarks(){
+	/**
+	 * returns total marks.
+	 * @return students marks.
+	 */
+	public Double getMarks() {
 		return marks;
 	}
-	Student(String studName, Double total){
+	/**
+	 * parameterized constructor.
+	 * @param studName [description]
+	 * @param total [description]
+	 */
+	Student(String studName, Double total) {
 		this.name = studName;
 		// this.rollNumber = rollNum;
 		this.marks = total;
